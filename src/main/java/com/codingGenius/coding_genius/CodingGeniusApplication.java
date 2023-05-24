e88d24c291c528d2684a5d61e096733c4f566d9d
DDonghyeo <gsu9045000@gmail.com>
2023-05-24 15:16:31 +0900

    dev init

---
+++ src/main/java/com/codingGenius/coding_genius/CodingGeniusApplication.java
@@ -0,0 +1,13 @@
+package com.codingGenius.coding_genius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingGeniusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingGeniusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingGenius/coding_genius/CodingGeniusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingGenius.coding_genius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingGeniusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
