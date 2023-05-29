61bb12de033dd10365831cbc7575b614467d7769
rlJzr <eopy0928@naver.com>
2023-05-29 16:16:26 +0900

    To Do List Drop Down메뉴 수정2

--- ToDoList/static/js/home.js
+++ ToDoList/static/js/home.js
@@ -1,10 +1,33 @@
-function toggleTaskDetails(button) {
-  var task = button.parentNode.parentNode;
-  var details = task.querySelector('.task-details');
-  details.classList.toggle('active');
-}
+//function toggleTaskDetails(button) {
+//  var task = button.parentNode.parentNode;
+//  var details = task.querySelector('.task-details');
+//  details.classList.toggle('active');
+//}
+//
+//function toggleTaskMemo(button) {
+//  var memo = button.nextElementSibling;
+//  memo.classList.toggle('active');
+//}
 
-function toggleTaskMemo(button) {
-  var memo = button.nextElementSibling;
-  memo.classList.toggle('active');
-}
+document.addEventListener('DOMContentLoaded', function() {
+  const taskToggleButtons = document.querySelectorAll('.task-toggle-button');
+  const memoToggleButtons = document.querySelectorAll('.memo-toggle-button');
+
+  taskToggleButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      const details = this.parentNode.parentNode.querySelector('.task-details');
+      details.classList.toggle('active');
+    });
+  });
+
+  memoToggleButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      const memo = this.parentNode.parentNode.parentNode.parentNode.querySelector('.task-memo');
+      memo.classList.toggle('active');
+    });
+  });
+});
+
+
+
+
